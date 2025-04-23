fa48cf1ec7a504dbb05a83836ebe4db010867786
Murod's Macbook <a1@Murods-Macbook.local>
2025-04-23 18:08:10 +0900

    feat: develop events page sectional component

--- src/app/screens/homePage/Events.tsx
+++ src/app/screens/homePage/Events.tsx
@@ -1,7 +1,8 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Autoplay, Navigation, Pagination } from "swiper";
 import { plans } from "../../../lib/data/plans";
+import Divider from "../../components/divider";
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -10,14 +11,16 @@ export default function Events() {
     <div className={"events-frame"}>
       <Stack className={"events-main"}>
         <Box className={"events-text"}>
-          <span className={"category-title"}>Events</span>
+          <Typography className="info-subt">everybody welcome</Typography>
+          <Typography className="info-title">Our Events</Typography>
+          <Divider width="2" height="40" bg="#DB9457" />
         </Box>
 
         <Swiper
           className={"events-info swiper-wrapper"}
           slidesPerView={"auto"}
           centeredSlides={true}
-          spaceBetween={30}
+          spaceBetween={48}
           navigation={{
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
@@ -27,7 +30,7 @@ export default function Events() {
             clickable: true,
           }}
           autoplay={{
-            delay: 2000,
+            delay: 3000,
             disableOnInteraction: true,
           }}
         >
@@ -40,26 +43,26 @@ export default function Events() {
                 <Box className={"events-desc"}>
                   <Box className={"events-bott"}>
                     <Box className={"bott-left"}>
+                      <div className={"bott-info"}>
+                        <Stack className={"bott-info-main"}>
+                          on {value.date}
+                        </Stack>
+                        <Stack className={"bott-info-main"}>
+                          <img src={"/icons/location.svg"} />
+                          {value.location}
+                        </Stack>
+                      </div>
+
                       <div className={"event-title-speaker"}>
-                        <strong>{value.title}</strong>
                         <div className={"event-organizator"}>
-                          <img src={"/icons/speaker.svg"} />
                           <p className={"spec-text-author"}>{value.author}</p>
                         </div>
+                        <strong className="spec-text-title">
+                          {value.title}
+                        </strong>
                       </div>
 
                       <p className={"text-desc"}> {value.desc} </p>
-
-                      <div className={"bott-info"}>
-                        <div className={"bott-info-main"}>
-                          <img src={"/icons/calendar.svg"} />
-                          {value.date}
-                        </div>
-                        <div className={"bott-info-main"}>
-                          <img src={"/icons/location.svg"} />
-                          {value.location}
-                        </div>
-                      </div>
                     </Box>
                   </Box>
                 </Box>
